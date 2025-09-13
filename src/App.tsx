9d7d4d71a04a06353ed60fa3ed0d176de0c43925
Felipe Arruda <felipearruda@MacBook-Air-de-Felipe.local>
2025-09-13 13:21:06 -0300

    feat: setup config

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="min-h-screen bg-gray-50 p-1">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
